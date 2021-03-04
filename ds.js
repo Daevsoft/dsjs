69c13134bdf34228da54377b3551338d3472a5f2
Developer Software <muhamad.deva.arofi@gmail.com>
2021-03-04 08:54:21 +0700

    Add files via upload

--- ds.js
+++ ds.js
@@ -1,31 +1,31 @@
 class Ds{
     app;
     data;
-
-    events = [
-        {
-            attr:"d-click",
-            trigger:"click"
-        },
-        {
-            attr:"d-mouseover",
-            trigger:"mouseover"
-        }
-    ];
-
+    event;
+    text(txt){
+        this.me.innerText = txt;
+    }
     render(selector, arg){
         this.app = document.getElementById(selector);
         this.data = arg.data;
-        this.events.forEach(ev => {
-            var el = this.app.querySelectorAll('*['+ev.attr+']');
-            el.forEach(element => {
-                var attrClick = element.attributes[ev.attr];
-                var attrValue = attrClick.nodeValue;
-                var attrValueClean = attrValue.substring(1,attrValue.length-1);
-                var elData = this.data;
-                elData.me = element;
-                element.addEventListener(ev.trigger, arg.event[attrValueClean].bind(this.data))
-            });
-        });
+        this.event = arg.event;
+        
+        for (let i = 0; i < this.app.children.length; i++) {
+            const el = this.app.children[i];
+            this.reRender(el);
+        }
+    }
+    reRender(element){
+        var elData = this.data;
+        elData.me = element;
+        for (let i = 0; i < element.attributes.length; i++) {
+            const attr = element.attributes[i];
+            const attrValue = attr.nodeValue;
+            const attrValueClean = attrValue.substring(1,attrValue.length-1);
+            if(attr.name[0] == '@'){
+                const event = attr.name.substring(1);
+                element.addEventListener(event, this.event[attrValueClean].bind(elData))
+            }
+        }
     }
 }
