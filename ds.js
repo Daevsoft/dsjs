7d900276857d9935accd3c342d5d2709e5847a95
Developer Software <muhamad.deva.arofi@gmail.com>
2021-03-05 09:51:18 +0700

    Add files via upload

--- ds.js
+++ ds.js
@@ -2,27 +2,74 @@ class Ds{
     app;
     data;
     event;
-    text(txt){
-        this.me.innerText = txt;
-    }
+    defaultEvent = [
+        {
+            eventName       :'text',
+            eventFunction   :function(txt){
+                this.me.innerText = txt;
+            }
+        },
+        {
+            eventName       :'html',
+            eventFunction   :function(subHtml){
+                this.me.innerHTML = subHtml;
+            }
+        },
+        {
+            eventName       :'attr',
+            eventFunction   :function(attributeName, attributeValue){
+                this.me.setAttribute(attributeName, attributeValue);
+            }
+        }
+    ];
+    
     render(selector, arg){
-        this.app = document.getElementById(selector);
+        this.app = {
+            children: document.querySelector('#'+selector).getElementsByTagName('*')
+        };
         this.data = arg.data;
         this.event = arg.event;
-        
+        for (let i = 0; i < this.app.children.length; i++) {
+            const el = this.app.children[i];
+            this.initData(el);
+        }
         for (let i = 0; i < this.app.children.length; i++) {
             const el = this.app.children[i];
             this.reRender(el);
         }
+        if(this.event.init !== undefined){
+            this.event.init.bind(this.data);
+            this.event.init.call(this.data);
+        }
     }
-    reRender(element){
-        var elData = {...this.data};
-        elData.me = element;
-        element.text = this.text.bind(elData);
+    refactorData(element, elData = {}){
+        elData = {me:element, ...elData};
+        for (let i = 0; i < this.defaultEvent.length; i++) {
+            const event = this.defaultEvent[i];
+            element[event.eventName] = event.eventFunction.bind(elData);
+            console.log(event.eventName)
+        }
+        return elData;
+    }
+    initData(element) {
+        this.refactorData(element);
         for (let i = 0; i < element.attributes.length; i++) {
             const attr = element.attributes[i];
             const attrValue = attr.nodeValue;
-            const attrValueClean = attrValue.substring(1,attrValue.length-1);
+            const attrValueClean = attrValue.substring(1,attrValue.length-1).trim();
+            if(attr.name[0] == 'd'){
+                if (attr.name == 'd-id') {
+                    this.data[attrValueClean] = element;
+                }
+            }
+        }
+    }
+    reRender(element){
+        var elData = this.refactorData(element, this.data);
+        for (let i = 0; i < element.attributes.length; i++) {
+            const attr = element.attributes[i];
+            const attrValue = attr.nodeValue;
+            const attrValueClean = attrValue.substring(1,attrValue.length-1).trim();
             if(attr.name[0] == '@'){
                 const event = attr.name.substring(1);
                 element.addEventListener(event, this.event[attrValueClean].bind(elData))
